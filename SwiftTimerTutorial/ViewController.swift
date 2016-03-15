924a621bc87ac8847b9a2dd3dc2b840b3bed4460
Keri Clowes <kclowes@quickleft.com>
2016-03-15 14:24:10 -0600

    Initial Commit

---
+++ SwiftTimerTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTimerTutorial
+//
+//  Created by Keri Clowes on 3/15/16.
+//  Copyright © 2016 Keri Clowes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
